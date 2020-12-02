7ca3ff48c696be107d4d9f477b4bede217145aea
Anoop <anoopchaudhary390@gmail.com>
2020-12-02 10:50:33 +0530

    Add comment in init.py file

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__, template_folder='template')
 app.config['SECRET_KEY'] = 'c65e9df7be789709bd30246305cab2a2'
-#Declaration of database sql lite
+#Declaration of database sql lite.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
