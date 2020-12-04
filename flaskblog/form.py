55882cb0997b7fe599c126333f8388fa1176023c
Anoop <anoopchaudhary390@gmail.com>
2020-12-04 16:45:44 +0530

    Update user datail on account page

--- flaskblog/form.py
+++ flaskblog/form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_login import current_user
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -35,5 +36,30 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        
+        if username.data != current_user.username:
+
+            user = User.query.filter_by(username=username.data).first()
+            print(user)
+            if user:
+                print('xxx')
+                raise ValidationError(
+                    'User name is taken please choose a different one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'Email Id is taken please choose a different one')
 
 
